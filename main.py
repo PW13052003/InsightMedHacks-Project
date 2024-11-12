9694dfa9ab99c6814a274c2b4b954a5cdb766084
PW13052003 <pw425@drexel.edu>
2024-11-12 13:23:07 -0500

    Adding comments

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+# Contributor(s): Puranjay Wadhera
+# Email: pw425@drexel.edu
+# Dataset used: cancer.csv
+    # Source: https://www.kaggle.com/datasets
+# Originally submitted to: Devpost (InsightMed Hacks Hackathon)
+
+# Import the necessary modules
+    # pandas is being used to read data
+    # matplotlib is being used to plot graphs
+    # seaborn is being used for finding correlations
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Read the file
 df = pd.read_csv("/Users/puranjaywadhera/Downloads/cancer.csv")
 
+# For statistical analysis and plotting graphs, we will only consider data attributes with a numerical data type
 numeric_data = df.select_dtypes(include=['number'])
 
 # Statistical analysis (Calculate mean, median, standard deviation and range)
@@ -11,7 +23,7 @@ mean_value = numeric_data.mean(numeric_only=True)
 median_value = numeric_data.median(numeric_only=True)
 standard_deviation_value = numeric_data.std(numeric_only=True)
 range_value = numeric_data.max() - numeric_data.min()
-
+# Print findings
 print("-----")
 print("\033[4mMEAN\033[0m:-\n" + str(round(mean_value, 2)))
 print("-----")
@@ -31,6 +43,7 @@ for column in numeric_data.columns:
     plt.ylabel('Frequency')
     plt.show()
 
+# Determine the correlations
 correlation_matrix = numeric_data.corr()
 
 # Plot the correlation matrix using a heatmap
