bcaf6c6d62f37ef4560d41567104825ea263b90f
PW13052003 <pw425@drexel.edu>
2024-11-12 12:44:53 -0500

    Statistical analysis of data

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("/Users/puranjaywadhera/Downloads/cancer.csv")
+
+# Statistical analysis (Calculate mean, median, standard deviation and range)
+mean_value = df.mean(numeric_only=True)
+median_value = df.median(numeric_only=True)
+standard_deviation_value = df.std(numeric_only=True)
+range_value = df.max() - df.min()
