f60833fb1e68147ee8b1288ecc28be55d06f846e
PW13052003 <pw425@drexel.edu>
2024-11-12 12:40:16 -0500

    Start analysis

--- main.py
+++ main.py
@@ -2,10 +2,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("/Users/puranjaywadhera/Downloads/cancer.csv")
-
-# Distribution analysis
-df['Age'].hist()
-plt.title('Age Distribution')
-plt.xlabel('Age')
-plt.ylabel('Frequency')
-plt.show()
