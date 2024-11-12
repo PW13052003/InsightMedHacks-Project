60a5fcb02e51edb9a45ab2f6d56e0d5f8071a775
PW13052003 <pw425@drexel.edu>
2024-11-12 12:57:14 -0500

    Mean, median, standard deviation and range values are successfully computed

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import pandas as pd
 
 df = pd.read_csv("/Users/puranjaywadhera/Downloads/cancer.csv")
 
+numeric_data = df.select_dtypes(include=['number'])
+
 # Statistical analysis (Calculate mean, median, standard deviation and range)
-mean_value = df.mean(numeric_only=True)
-median_value = df.median(numeric_only=True)
-standard_deviation_value = df.std(numeric_only=True)
-range_value = df.max() - df.min()
+mean_value = numeric_data.mean(numeric_only=True)
+median_value = numeric_data.median(numeric_only=True)
+standard_deviation_value = numeric_data.std(numeric_only=True)
+range_value = numeric_data.max() - numeric_data.min()
+
+print("-----")
+print("\033[4mMEAN\033[0m:-\n" + str(round(mean_value, 2)))
+print("-----")
+print("\033[4mMEDIAN\033[0m:-\n" + str(median_value))
+print("-----")
+print("\033[4mSTANDARD DEVIATION\033[0m:-\n" + str(round(standard_deviation_value, 2)))
+print("-----")
+print("\033[4mRANGE\033[0m:-\n" + str(round(range_value, 2)))
+print("-----")
